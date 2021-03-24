18159104b97ca78506a88c95ea7af1dfc8db13df
PrabhKamboj <81244246+PrabhKamboj@users.noreply.github.com>
2021-03-24 10:10:31 +0530

    Update product.js

--- products/product.js
+++ products/product.js
@@ -12,20 +12,22 @@ function checkavailability(){
 	var arr = [147001,147111,147203,140406,143001,140118];
 	var pincode=document.getElementById("delivery").value;
 	var time = new Date(new Date().getTime() + 4*60*60*1000).toLocaleTimeString();
-	var regex = /^[a-zA-Z !@#$%\^&*)(+=._-]*$/
+	var regex = /^[a-zA-Z0-9!@#$%\^&*)(+=._-]*$/
+	var regexnum = /^[0-9]*$/
 	for(i=0;i<arr.length;i++){
 		if(pincode==arr[i]){
 			document.getElementById("output").innerHTML="<b>Delivery in 2 Days | Free <strike>₹40</strike></b> <br> Only if you order before "+time;
 			document.getElementById("output").style.color="#198754";
 			break
 		}
-		else if(pincode==""|| regex.test(pincode)){document.getElementById("output").innerHTML="<b>Not a valid pincode<b/>";
+		else if(regexnum.test(pincode)){document.getElementById("output").innerHTML="<b>Sorry, We are expanding,<br> We will be there soon</b>";
+		document.getElementById("output").style.color="red";
+	}
+		else if(pincode==""|| regex.test(pincode)||pincode.length<6){document.getElementById("output").innerHTML="<b>Not a valid pincode<b/>";
 		document.getElementById("output").style.color="red";
 	}
 
-		else{document.getElementById("output").innerHTML="<b>Sorry, We are expanding,<br> We will be there soon</b>";
-		document.getElementById("output").style.color="red";
-	}
+		
 
 }
 }
